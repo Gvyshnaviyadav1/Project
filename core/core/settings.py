850449a2f8bf4397d8da5d9c75cca934fdc9e34d
Gvyshnaviyadav1 <vyshnavi.yadav.23031@iitgoa.ac.in>
2025-06-22 16:18:51 +0530

    commit

--- core/core/settings.py
+++ core/core/settings.py
@@ -80,10 +80,16 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Onlinej',            # Your database name
+        'USER': 'postgres',  # Replace with your PostgreSQL username
+        'PASSWORD': 'vymu', # Replace with your PostgreSQL password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
+    
 }
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
