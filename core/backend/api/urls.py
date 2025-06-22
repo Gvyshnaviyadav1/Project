704f33a299048465acc185b1d673b9ebf82acd21
Gvyshnaviyadav1 <vyshnavi.yadav.23031@iitgoa.ac.in>
2025-06-22 17:13:21 +0530

    commit

--- core/backend/api/urls.py
+++ core/backend/api/urls.py
@@ -2,8 +2,11 @@
 from django.urls import path
 from backend.views import LoginView
 from backend.views import register_user
+from backend.views import ProblemListView, ProblemDetailView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', register_user),
+    path('problems/', ProblemListView.as_view(), name='problem-list'),
+    path('problems/<int:pk>/', ProblemDetailView.as_view(), name='problem-detail'),
 ]
